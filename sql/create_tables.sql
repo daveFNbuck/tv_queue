68c3baba3aea18386c219cf9a8d7af6bf2840f77
Dave Buchfuhrer <dbuchfuhrer@gmail.com>
2016-12-02 09:58:44 -0800

    Queries for initializing the databases

---
+++ sql/create_tables.sql
@@ -0,0 +1,55 @@
+CREATE TABLE `user` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `series` (
+  `id` bigint(20) unsigned NOT NULL,
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `air_time` time DEFAULT NULL,
+  `episode_length` int(11) DEFAULT NULL,
+  `last_updated` bigint(20) NOT NULL,
+  `network` varchar(20) DEFAULT NULL,
+  `banner` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `episode` (
+  `id` bigint(20) unsigned NOT NULL,
+  `series_id` bigint(20) unsigned NOT NULL,
+  `season` int(11) NOT NULL,
+  `episode` int(11) NOT NULL,
+  `title` varchar(50) DEFAULT '',
+  `air_date` date DEFAULT NULL,
+  `overview` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `show_season_episode` (`series_id`,`season`,`episode`),
+  KEY `fk_show_id` (`series_id`),
+  CONSTRAINT `episode_ibfk_1` FOREIGN KEY (`series_id`) REFERENCES `series` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `subscription` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `series_id` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `subscription` (`user_id`,`series_id`),
+  KEY `series_id` (`series_id`),
+  CONSTRAINT `subscription_ibfk_2` FOREIGN KEY (`series_id`) REFERENCES `series` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `unseen` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `episode_id` bigint(20) unsigned NOT NULL,
+  `subscription_id` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `episode_id` (`episode_id`),
+  KEY `subscription_id` (`subscription_id`),
+  CONSTRAINT `unseen_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `unseen_ibfk_2` FOREIGN KEY (`episode_id`) REFERENCES `episode` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `unseen_ibfk_3` FOREIGN KEY (`subscription_id`) REFERENCES `subscription` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;

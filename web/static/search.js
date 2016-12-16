dfe733b5430f3f6743dedef98e573eb9fbec5a4b
Dave Buchfuhrer <dbuchfuhrer@gmail.com>
2016-12-15 21:09:31 -0800

    Uses DataTables lib that doesn't provide jQuery

--- web/static/search.js
+++ web/static/search.js
@@ -32,7 +32,6 @@ $(function() {
         var series_id = this.attributes['data-series-id'].nodeValue;
         var data = {'series_id': series_id}
         if (this.textContent == "Unsubscribe") {
-            jQuery.noConflict();
             $('.modal[for_series=' + series_id + ']').modal()
         } else if (this.textContent == "Subscribe") {
             $.ajax('/subscribe', {'success': handle_subscription, 'error': fail_subscription, 'data': data})
